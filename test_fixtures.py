c05799602ecf569061bc98f5717386b2ce84211a
Daniil Matafonov <danielmt.dm@gmail.com>
2022-08-04 15:48:03 +0300

    github sign in tests

---
+++ test_fixtures.py
@@ -0,0 +1,30 @@
+import pytest
+from selene.support.by import link_text
+from selene.support.shared import browser
+
+url = 'https://github.com/'
+browserName = 'chrome'
+
+@pytest.fixture(scope='function')
+def desktop_func():
+    browser.config.browser_name = browserName
+    browser.config.window_width = 1360
+    browser.config._window_height = 768
+
+
+@pytest.fixture(scope='function')
+def mobile_func():
+    browser.config.browser_name = browserName
+    browser.config.window_width = 720
+    browser.config._window_height = 1080
+
+
+def test_github_desktop(desktop_func):
+    browser.open(url)
+    browser.element(link_text('Sign in')).click()
+
+
+def test_github_mobile(mobile_func):
+    browser.open(url)
+    browser.element('[class="octicon octicon-three-bars"]').click()
+    browser.element(link_text('Sign in')).click()
